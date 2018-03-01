ec302e1798dbc43e6b0a9f51dde78291780c9a4e
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-03-01 17:25:09 +0530

    Create roma.cpp

---
+++ roma.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+int main()
+{
+ int romanToInt(String n)
+ int e=0;
+ map<char,int> roman;
+ roman['I']=1;
+ roman['V']=5;
+ roman['X']=10;
+ for(i=0;i<n.length()-1;i++)
+ {
+  int pre=roman[n(i)];
+  int next=roman[n(i+1)];
+  if(pre>=next)
+  {
+   V+=pre;
+  }
+  else
+  {
+   e-=pre;
+  }
+  return e roman[n(n.length()-1)];
+  return;
+  }
