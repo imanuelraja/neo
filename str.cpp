1f22981aa8364e9efd76b916bf7b7a73f95a5687
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-03-02 17:25:52 +0530

    Create str.cpp

---
+++ str.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+int main()
+{
+ char* a,b;
+ int i,j,count=o;
+ cin>>a;
+ cin>>b;
+ if(a==b)
+ {
+  count++;
+ } 
+ if(count==1)
+ {
+  cout<<"no";
+ }
+ else
+ {
+  cout<<"yes";
+ }
+} 
